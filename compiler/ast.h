7574638ff678ed676a6a2f10f9d55b49754d0369
SayMouz <faouz5307@gmail.com>
2021-03-24 09:53:05 +0100

    ast.h v1 fini

--- compiler/ast.h
+++ compiler/ast.h
@@ -3,10 +3,9 @@
 
 #include <string>
 #include <vector>
-
-class Instr;
-class Block;
-class Expr;
+#include "./symbol-table/Context.h" 
+#include "expr.h"
+using namespace std;
 
 typedef enum {
     int64,
@@ -15,55 +14,110 @@ typedef enum {
 
 typedef enum {
     AFF,
-    DECL,
+//    DECL,
     DECLAFF,
-    RET
+    RET,
+    IF,
+    WHILE,
+    FOR,
+    FUNCCALL
 } Instruction;
+
+class Block {
+    public :
+        Block(Context* c) : context(c) {}
+        void addInstr(Instr instr);
+        vector<Instr> instrs;
+        Context* context;
+};
 
 class Func {
     public : 
-        Func(int t, string n) : type(t), name(n) {}
-        void addParam(Decl param);
+        Func(int t, string n, Context* c) : type(t), name(n), block(Block(c)) {}
+        void addParam(string param);
         void addInstr(Instr instr);
 
         int type;
         string name;
-        vector<Decl> params;
-        vector<Instr> block;
+        vector<string> params;
+        Block block;
 };
+
+
 
 
 class Instr {
-    Instr(int inst) : instruction(inst) {}
-    int instruction;
+    public :
+        Instr(Instruction inst) : instruction(inst) {}
+        int instruction;
 };
 
 
 
-class Aff : public Instr {
-    Aff(string id, Expr ex) : Instr(AFF), varId(id), expr(ex) {}
-    string varId;
-    Expr expr;
+class Aff : public Instr, public Expr {
+    public :
+        Aff(string id, Expr ex) : Instr(AFF), varId(id), expr(ex) {}
+        string varId;
+        Expr expr;
 };
 
+/*
 class Decl : public Instr {
     Decl(int t, string id) : Instr(DECL), type(t), varId(id) {}
     int type;
     string varId;
 };
+*/
 
-class DeclAff : public Decl {
-    DeclAff(int type, string id, Expr ex) : Instr(DECLAFF), Decl(t,id), expr(ex) {}
-    Expr expr;
+class DeclAff : public Instr {
+    public :
+        DeclAff(string id, Expr ex) : Instr(DECLAFF), varId(id), expr(ex) {}
+        string varId;
+        Expr expr;
 };
 
 class Ret : public Instr {
-    Ret(Expr ex): Instr(RET), expr(ex) {}
-    Expr expr;
-}
+    public :
+        Ret(Expr ex): Instr(RET), expr(ex) {}
+        Expr expr;
+};
 
+class If : public Instr {
+    public :
+        If(Expr cond, Context* cif, Context* celse): Instr(IF), condition(cond), 
+            blockIf(Block(cif)), blockElse(Block(celse)){ }
+        void addInstrIf(Instr instr);
+        void addInstrElse(Instr instr);    
+        Expr condition;
+        Block blockIf;
+        Block blockElse;
+};
 
+class While : public Instr {
+    public :
+        While(Expr cond, Context* c): Instr(WHILE), condition(cond), block(Block(c)){}
+        void addInstr(Instr instr);   
+        Expr condition;
+        Block block;
+};
 
+class For : public Instr {
+    public :
+        For(Expr i, Expr cond, Expr p, Context* c) : Instr(FOR), init(i), condition(cond), progression(p), block(Block(c)) {}
+        void addInstr(Instr instr);  
+        Expr init;
+        Expr condition;
+        Expr progression;
+        Block block;
+};
+
+class FuncCall : public Instr, public Expr {
+    public :
+        FuncCall(string fn): Instr(FUNCCALL), funcName(fn) {}
+        void addParam(Expr param);  
+        string funcName;
+        vector<Expr> params;
+};
 
 
 #endif
--- compiler/expr.h
+++ compiler/expr.h
@@ -15,25 +15,26 @@ class Expr {
 
 };
 
-class BinOpMdExpr : public Expr{
-
-    Expr expr1;
-    Expr expr2;
-    int operation;
+class BinOp : public Expr{
+    public :
+        BinOp(Expr e1, Expr e2, int op) : expr1(e1), expr2(e2), operation(op) {}
+        Expr expr1;
+        Expr expr2;
+        int operation;
 };
 
 
 class ConstExpr : public Expr {
-
-    ConstExpr(string v) : val(v) {}
-    string val;
-}
+    public :
+        ConstExpr(string v) : val(v) {}
+        string val;
+};
 
 
 class VarExpr : public Expr {
-
-    VarExpr(string v) : varName(v) {}
-    string varName;
-}
+    public :
+        VarExpr(string v) : varName(v) {}
+        string varName;
+};
 
 #endif
