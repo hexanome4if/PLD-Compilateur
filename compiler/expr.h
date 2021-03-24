4d0dfa8bdedad67b5cef3513d68de1c050be7d15
SayMouz <faouz5307@gmail.com>
2021-03-24 08:39:33 +0100

    update ast

--- compiler/expr.h
+++ compiler/expr.h
@@ -16,6 +16,7 @@ class Expr {
 };
 
 class BinOpMdExpr : public Expr{
+
     Expr expr1;
     Expr expr2;
     int operation;
@@ -23,7 +24,16 @@ class BinOpMdExpr : public Expr{
 
 
 class ConstExpr : public Expr {
+
+    ConstExpr(string v) : val(v) {}
     string val;
+}
+
+
+class VarExpr : public Expr {
+
+    VarExpr(string v) : varName(v) {}
+    string varName;
 }
 
 #endif
