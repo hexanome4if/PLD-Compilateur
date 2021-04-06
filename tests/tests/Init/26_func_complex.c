7472a665619906253f193a286e09d8cc79ab75ba
Fabien Narboux <narbouxfabien42@hotmail.fr>
2021-04-06 18:58:58 +0200

    test factoriel ok

--- tests/tests/Init/26_func_complex.c
+++ tests/tests/Init/26_func_complex.c
@@ -1,20 +1,21 @@
 int g()
 {
-	int a;
+				int a;
+				return 1;
 }
 
 int func(int a, int b, int c, int d, int e, int f)
 {
-	int t = 1;
-	g();
-	return a + b + c + t;
+				int t = 1;
+				g();
+				return a + b + c + t;
 }
 
 int main()
 {
-	int a = 5;
+				int a = 5;
 
-	int ret = func(5, 6, 7, 8, 9, 12);
+				int ret = func(5, 6, 7, 8, 9, 12);
 
-	return ret + a;
+				return ret + a;
 }
