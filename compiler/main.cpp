518f23c8b96bf7a845fbc3e1b73038aa0d997674
Polo <pablo_mo@orange.fr>
2021-04-07 09:14:05 +0200

    CLI params

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -22,107 +22,118 @@ using namespace std;
 
 void standardFunctions(SymbolTable *symbolTable)
 {
-				FuncSymbol *putChar = new FuncSymbol("putchar", "int");
-				putChar->used();
-				symbolTable->addSymbol(putChar);
-				FuncSymbol *getChar = new FuncSymbol("getchar", "int");
-				getChar->used();
-				symbolTable->addSymbol(getChar);
+	FuncSymbol *putChar = new FuncSymbol("putchar", "int");
+	putChar->used();
+	symbolTable->addSymbol(putChar);
+	FuncSymbol *getChar = new FuncSymbol("getchar", "int");
+	getChar->used();
+	symbolTable->addSymbol(getChar);
 }
 
-void computeParameters(int argn, const char **argv, stringstream& in, map<string, bool>& parameters)
+void computeParameters(int argn, const char **argv, stringstream &in, map<string, bool> &parameters)
 {
-				parameters["optimization"] = false;
-
-				for(int i = 1; i < argn; ++i)
-				{
-								string val = string(argv[i]);
-								if (val[0] == '-')
-								{
-												if (val == "-O")
-												{
-																parameters["optimization"] = true;
-												}
-								}
-								else
-								{
-												ifstream lecture(argv[i]);
-												in << lecture.rdbuf();
-								}
-				}
+	parameters["x86"] = true;
+	parameters["optimization"] = false;
+	parameters["ast"] = false;
+	for (int i = 1; i < argn; ++i)
+	{
+		string val = string(argv[i]);
+		if (val[0] == '-')
+		{
+			if (val == "-O")
+			{
+				parameters["optimization"] = true;
+			}
+			else if (val == "-AST")
+			{
+				parameters["x86"] = false;
+				parameters["ast"] = true;
+			}
+		}
+		else
+		{
+			ifstream lecture(argv[i]);
+			in << lecture.rdbuf();
+		}
+	}
 }
 
 int main(int argn, const char **argv)
 {
-				map<string, bool> parameters;
-				stringstream in;
+	map<string, bool> parameters;
+	stringstream in;
 
-				computeParameters(argn, argv, in, parameters);
+	computeParameters(argn, argv, in, parameters);
 
-				ANTLRInputStream input(in.str());
-				ifccLexer lexer(&input);
-				CommonTokenStream tokens(&lexer);
+	ANTLRInputStream input(in.str());
+	ifccLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
 
-				tokens.fill();
-				if (lexer.getNumberOfSyntaxErrors() > 0)
-								return 1;
-				ifccParser parser(&tokens);
-				tree::ParseTree *tree = parser.axiom();
-				if (parser.getNumberOfSyntaxErrors() > 0)
-								return 1;
+	tokens.fill();
+	if (lexer.getNumberOfSyntaxErrors() > 0)
+		return 1;
+	ifccParser parser(&tokens);
+	tree::ParseTree *tree = parser.axiom();
+	if (parser.getNumberOfSyntaxErrors() > 0)
+		return 1;
 
-				SymbolTable *symbolTable = new SymbolTable();
+	SymbolTable *symbolTable = new SymbolTable();
 
-				standardFunctions(symbolTable);
+	standardFunctions(symbolTable);
 
-				SymbolVisitor symbolVisitor(symbolTable);
-				symbolVisitor.visit(tree);
-				symbolTable->setCreationDone();
+	SymbolVisitor symbolVisitor(symbolTable);
+	symbolVisitor.visit(tree);
+	symbolTable->setCreationDone();
 
-				ErrorManager *symbolErrorManager = symbolVisitor.getErrorManager();
-				if (symbolErrorManager->hasErrorOrWarning())
-				{
-								symbolErrorManager->showAll(cerr);
-								if (symbolErrorManager->hasError())
-								{
-												return 1;
-								}
-				}
+	ErrorManager *symbolErrorManager = symbolVisitor.getErrorManager();
+	if (symbolErrorManager->hasErrorOrWarning())
+	{
+		symbolErrorManager->showAll(cerr);
+		if (symbolErrorManager->hasError())
+		{
+			return 1;
+		}
+	}
 
-				ErrorVisitor errorVisitor;
-				errorVisitor.visit(tree);
-				ErrorManager *errorManager = errorVisitor.getErrorManager();
-				if (errorManager->hasErrorOrWarning())
-				{
-								errorManager->showAll(cerr);
-								if (errorManager->hasError())
-								{
-												return 1;
-								}
-				}
+	ErrorVisitor errorVisitor;
+	errorVisitor.visit(tree);
+	ErrorManager *errorManager = errorVisitor.getErrorManager();
+	if (errorManager->hasErrorOrWarning())
+	{
+		errorManager->showAll(cerr);
+		if (errorManager->hasError())
+		{
+			return 1;
+		}
+	}
 
-				Ast *ast = new Ast();
-				AstVisitor astVisitor(ast, symbolTable);
-				astVisitor.visit(tree);
+	Ast *ast = new Ast();
+	AstVisitor astVisitor(ast, symbolTable);
+	astVisitor.visit(tree);
 
-				if (parameters["optimization"])
-				{
-								ast->removeUnusedSymbols(symbolTable);
-								ast->calculateExpressions(symbolTable);
-				}
-				// ast->debug(cout);
+	if (parameters["optimization"])
+	{
+		ast->removeUnusedSymbols(symbolTable);
+		ast->calculateExpressions(symbolTable);
+	}
+	ast->removeUnusedSymbols(symbolTable);
+	ast->calculateExpressions(symbolTable);
 
+	if (parameters["ast"])
+	{
+		ast->debug(cout);
+	}
 
+	CFG *cfg = new CFG(ast, symbolTable);
+	IRGenerator *irGenerator = new IRGenerator(ast, cfg, symbolTable);
+	irGenerator->generate();
+	symbolTable->assignMemoryAddresses();
 
-				CFG *cfg = new CFG(ast, symbolTable);
-				IRGenerator *irGenerator = new IRGenerator(ast, cfg, symbolTable);
-				irGenerator->generate();
-				symbolTable->assignMemoryAddresses();
-				X86Translator *translator = new X86Translator(cout, cfg);
-				translator->translate();
+	if (parameters["x86"])
+	{
+		X86Translator *translator = new X86Translator(cout, cfg);
+		translator->translate();
+	}
 
-
-
-
-				return 0;
+	return 0;
 }
